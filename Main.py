8e98e9531b2074f5bd9737c1e1f797b9d451c362
Iwan <vikingiwan@gmail.com>
2022-03-09 08:01:10 +0000

    Slash command migration (#1)
    * init cleanup
    
    * Migrated poke & quote command, as well as redefine user to member
    
    * migrated flip
    
    * migrated hug
    
    * migrated roll
    
    * Migrated promote to slash
    
    * Cleanup

--- Main.py
+++ Main.py
@@ -27,8 +27,12 @@ global botID
 botID = 217108205627637761
 global vtacGuild
 vtacGuild = [183107747217145856]
+global VTAC
+VTAC = 183107747217145856
 global mainChannel
 mainChannel= 622144477233938482
+global logChannel
+logChannel = 951018686867701790
 
 intents = nextcord.Intents.default()
 intents.members = True
@@ -85,8 +89,6 @@ rankClass_enlisted = [rank_pvt, rank_pfc, rank_spc]
 rankClass_subcommand = [rank_cpl, rank_sgt, rank_sgm]
 rankClass_command = [rank_2lt, rank_1lt, rank_cpt]
 rankClass_highcommand = [rank_maj, rank_col, rank_gen, rank_com]
-
-
 ##########
 
 
@@ -127,7 +129,7 @@ def getRankClass(member):
     return _rankClass
 
 def getRankObj(rank):
-    bot.get_guild(vtacGuild).get_role(rank)
+    bot.get_guild(VTAC).get_role(rank)
     return rank
 
 
@@ -177,8 +179,8 @@ def getPromoRank(member):
         elif r.id == rank_rec:
             _curRank = rank_rec
             _promoRank = rank_pvt
-    _curRank = bot.get_guild(vtacGuild).get_role(_curRank)
-    _promoRank = bot.get_guild(vtacGuild).get_role(_promoRank)
+    _curRank = bot.get_guild(VTAC).get_role(_curRank)
+    _promoRank = bot.get_guild(VTAC).get_role(_promoRank)
     return _curRank, _promoRank
     
 def debug(msg):
@@ -204,22 +206,13 @@ def get_quote():
     quote = str(quote)
     quote = quote.strip("('',)")
     return quote
-    
-def get_changelog(ver):
-    with open ('changelogs/' + ver + '.txt', 'r') as changelog:
-        changelog = changelog.read()
-        changelog = changelog.splitlines()
-    changelog = str(changelog)
-    changelog = changelog.replace("',", "\n")
-    changelog = changelog.split("['],")
-    return changelog
 
     
 
 #Bot Events
 @bot.event
 async def on_ready():
-    print("nextcord.py version: " + nextcord.__version__)
+    print("nextcord version: " + nextcord.__version__)
     print("Logged in as: " + bot.user.name)
     print("ID: " + str(bot.user.id))
     print("------------------")
@@ -229,72 +222,26 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(member.name + " has joined the guild...assigning rank...")
-    _role = bot.get_guild(vtacGuild).get_role(rank_rec)
+    _role = bot.get_guild(VTAC).get_role(rank_rec)
     await member.add_roles(_role, reason="New member", atomic=True)
     print("Recruit rank added to " + member.display_name)
     print("Adding rank prefix...")
     _nick = "Rec. " + member.name
     await member.edit(nick=_nick, reason="New User")
     print("Added prefix to " + member.display_name)
-    _chan = bot.get_guild(vtacGuild).get_channel(mainChannel)
-    await _chan.send(":thumbsup: " + member.mention + " has joined Viking Tactical.")
+    _chan = bot.get_guild(VTAC).get_channel(mainChannel)
     await member.send(welcome_message)
 
-#OPERATOR ONLY COMMANDS:
-@bot.command(pass_context = True)
-async def say(ctx, *, msg: str):
-    if getRankClass(ctx.author) == "highcommand":
-        await ctx.message.delete()
-        await ctx.channel.send(msg)
-    else:
-        await bot.say("ERROR: UNAUTHORIZED!")
-
-@bot.command(pass_context = True)
-async def purge(ctx):
-    if getRankClass(ctx.author) == "highcommand":
-        await ctx.send("UNDERSTOOD, COMMANDER. I WILL DESTROY THE EVIDENCE!")
-        await asyncio.sleep(4)
-        await ctx.channel.purge(limit=100, bulk=True)
-        await ctx.send("CHANNEL HAS BEEN PURGED, SIR!")
-    else:
-        await ctx.send("ERROR: UNAUTHORIZED")
-        
-@bot.command(pass_context = True)
-async def getBot(ctx):
-    if getRankClass(ctx.author) == "highcommand":
-        await ctx.message.delete()
-        await ctx.author.send("Invite link:\nhttps://discordapp.com/api/oauth2/authorize?client_id=217108205627637761&scope=bot&permissions=1")
-    else:
-        await ctx.author("ERROR: UNAUTHORIZED!")
-        
-@bot.command(pass_context = True)
-async def terminate(ctx):
-    if getRankClass(ctx.author) == "highcommand":
-        await ctx.author.send("Affirmative. Terminating now...")
-        sys.exit()
-    else:
-        await ctx.author.send("ERROR: UNAUTHORIZED!")
-        
-#OFFICER COMMANDS
 
 
-@bot.command(pass_context = True)
-async def martial(ctx, member: nextcord.Member=None, *, reason: str=None):
-    if getRankClass(ctx.author) == "command" or "highcommand":
-        if member == None or reason == None:
-            await ctx.channel.send("Please provide a member and reason.")
-        else:
-            await ctx.message.delete()
-            _waitmsg = await ctx.channel.send("Creating paperwork for new Court-Martial, please wait...")
-            await newCourtMartial(ctx.message.author, member, reason)
-            await _waitmsg.delete()
-    else:
-        await ctx.channel.send("ERROR: UNAUTHORIZED!")
+### Admin Commands ###
 
+### Officer Commands ###
 
-@bot.command(pass_context = True)
-async def promote(ctx, *, member: nextcord.Member = None):
-    if getRankClass(ctx.author) == "subcommand" or "command" or "highcommand":
+@bot.slash_command(name="promote", description="promote a user", guild_ids=vtacGuild)
+async def promote(interaction: Interaction, member:nextcord.User = nextcord.SlashOption(name="user", description="User to promote", required=True)):
+    sender = interaction.user
+    if getRankClass(sender) == "subcommand" or "command" or "highcommand":
         curRank, promoRank = getPromoRank(member)
         debug("\n" + "Member: " + member.display_name + "\n" + "Rank: " + curRank.name + "\n" + "Promo rank: " + promoRank.name)
         #Main Rank Work:
@@ -304,25 +251,25 @@ async def promote(ctx, *, member: nextcord.Member = None):
         await member.remove_roles(curRank, reason="Promotion", atomic=True)
         if promoRank.id == rank_pvt:
             debug("New class - Enlisted")
-            _rank = bot.get_guild(vtacGuild).get_role(rank_enlisted)
+            _rank = bot.get_guild(VTAC).get_role(rank_enlisted)
             await member.add_roles(_rank, reason="Promotion", atomic=True)
         elif promoRank.id == rank_cpl:
             debug("New Class - Subcommand")
-            _rank = bot.get_guild(vtacGuild).get_role(rank_subcommand)
+            _rank = bot.get_guild(VTAC).get_role(rank_subcommand)
             await member.add_roles(_rank, reason="Promotion to SubCommand", atomic=True)
-            _rank = bot.get_guild(vtacGuild).get_role(rank_enlisted)
+            _rank = bot.get_guild(VTAC).get_role(rank_enlisted)
             await member.remove_roles(_rank, reason="Promotion to SubCommand", atomic=True)
         elif promoRank.id == rank_2lt:
             debug("New Class - Command")
-            _rank = bot.get_guild(vtacGuild).get_role(rank_command)
+            _rank = bot.get_guild(VTAC).get_role(rank_command)
             await member.add_roles(_rank, reason="Promotion to Command", atomic=True)
-            _rank = bot.get_guild(vtacGuild).get_role(rank_subcommand)
+            _rank = bot.get_guild(VTAC).get_role(rank_subcommand)
             await member.remove_roles(_rank, reason="Promotion to Command", atomic=True)
         elif promoRank.id == rank_maj:
             debug("New Class - High-Command")
-            _rank = bot.get_guild(vtacGuild).get_role(rank_highcommand)
+            _rank = bot.get_guild(VTAC).get_role(rank_highcommand)
             await member.add_roles(_rank, reason="Promotion to High-Command", atomic=True)
-            _rank = bot.get_guild(vtacGuild).get_role(rank_command)
+            _rank = bot.get_guild(VTAC).get_role(rank_command)
             await member.remove_roles(_rank, reason="Promotion to High-Command", atomic=True)
         else:
             debug("No new class to process")
@@ -354,141 +301,39 @@ async def promote(ctx, *, member: nextcord.Member = None):
         await member.edit(nick=_nick, reason="Promotion")
 
         #Post to log channel
-        await bot.get_guild(vtacGuild).get_channel(logChannel).send("```" + "\n" + ctx.author.display_name + " has promoted " + member.name + " from " + curRank.name + " to " + promoRank.name + "\n" + "```")
+        await interaction.response.send_message("```" + member.name + " has been promoted to " + promoRank.name + "```")
+        await bot.get_guild(VTAC).get_channel(logChannel).send("```" + "\n" + sender.display_name + " has promoted " + member.name + " from " + curRank.name + " to " + promoRank.name + "\n" + "```")
 
     else:
-        await ctx.channel.send("ERROR: UNAUTHORIZED!")
-        
-#USER COMMANDS
-@bot.command(pass_context = True)
-async def help(ctx):
-    usrCmds = '\n'.join("!" + str(c) for c in userCommands)
-    em = nextcord.Embed(title='', description=usrCmds, colour=0xFF0000)
-    em.set_author(name='Commands:', icon_url=bot.user.avatar_url)
-    await ctx.message.channel.send(embed=em)
-
-@bot.command()
-async def version(ctx):
-    await ctx.channel.send("I am currently on version " + VERSION)
-    
-@bot.command(pass_context = True)
-async def changelog(ctx, ver: str=VERSION):
-    await ctx.channel.send("Changelog for version " + ver + ":")
-    for x in get_changelog(ver):
-        await ctx.channel.send("`" + str(x).strip("['],").replace("'", "") + "`")
-    
-@bot.command(pass_context = True)
-async def hug(ctx):
-    hug = random.choice([True, False])
-    if hug == True:
-        await ctx.channel.send(ctx.message.author.mention + ": :hugging:")
-    else:
-        await ctx.channel.send(ctx.message.author.mention + ": You don't deserve a hug, cyka.")
-        
-@bot.command()
-async def roll(ctx, *, dice : str=None):
-    if dice == None:
-        await ctx.channel.send('Format has to be in NdN!')
-        return
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.channel.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.channel.send(result)
-      
-@bot.command(pass_context = True)
-async def flip(ctx):
-    await ctx.channel.send("Okay, I'll flip it!")
-    await asyncio.sleep(3)
-    if random.choice([True, False]) == True:
-        await ctx.channel.send(ctx.message.author.mention + ": the result is.......**HEADS**!")
-    else:
-        await ctx.channel.send(ctx.message.author.mention + ": the result is.......**TAILS**!")
-      
-@bot.group(pass_context = True)
-async def remind(ctx, time: str = "0", *, reminder: str="null"):
-    time = int(time)
-    if time == 0 or reminder == "null":
-        await ctx.channel.send("Correct Usage: !remind <time in minutes> <reminder>")
-        await ctx.channel.send("Example: !remind 5 Tell me how reminders work")
-        return
-    else:
-        await ctx.message.delete()
-        await ctx.channel.send("Okay, " + ctx.message.author.mention + "! I'll remind you :smile:")
-        await asyncio.sleep(time * 60)
-        await ctx.message.author.send("You wanted me to remind you: " + reminder)
-        
-@bot.command(pass_context = True)
-async def kill (ctx, *, member: nextcord.Member = None):
-    if member is None:
-        await ctx.channel.send(ctx.message.author.mention + ": I need a target!")
-        return
-
-    if member.id == botID and ctx.message.author.id == iwanID:
-        await ctx.channel.send(ctx.message.author.mention + ": C-Commander, p-please...I'm useful! Please don't terminate me! :cry:")
-    elif member.id == ctx.message.author.id:
-        await ctx.channel.send(ctx.message.author.mention + ": Why do you want me to kill you? :open_mouth:")
-    elif member.id == botID:
-        await ctx.channel.send(ctx.message.author.mention + ": Hah! Don't get cocky kid, I could end you in less than a minute! :dagger:")
-    elif member.id == iwanID:
-        await ctx.channel.send(ctx.message.author.mention + ": Kill the Commander? I could never!")
-    else:
-        random.seed(time.time())
-        choice = killResponses[random.randrange(len(killResponses))] % member.mention
-        await ctx.channel.send(ctx.message.author.mention + ": " + choice)
-      
-@bot.command(pass_context = True)
-async def pat(ctx, *, member: nextcord.Member = None):
-    if member is None:
-        await ctx.channel.send("Aww, does somebody need a headpat? I'll pat you, " + ctx.message.author.mention)
-        await ctx.channel.send(file=nextcord.File("img/headpat.gif"))
-    else:
-        await ctx.channel.send(ctx.message.author.mention + " pats " + member.mention)
-        await ctx.channel.send(file=nextcord.File("img/headpat.gif"))
-       
-@bot.command()
-async def quote(ctx):
-    await ctx.channel.send(get_quote())
-    
-@bot.command(pass_context = True)
-async def poke(ctx, member: nextcord.Member=None):
-    if member==None:
-        await ctx.channel.send("I can't poke nobody! Try mentioning someone with `@`, like this\n`!poke @Iwan`")
-        return
-    else:
-        await ctx.channel.send(ctx.message.author.mention + " just poked " + member.mention + "!")
-        await ctx.channel.send(file=nextcord.File("img/poke.gif"))
+        await interaction.response.send_message("ERROR: UNAUTHORIZED!")
 
 
 
+### User Commands ###
 
-    
-# Slash Command testing
+# Test Command
 @bot.slash_command(name = "test", description= "This is a test slash command!", guild_ids=vtacGuild)
 async def test(interaction: Interaction):
     await interaction.response.send_message("Hello World")
 
 @bot.slash_command(name="pfp", description="Returns the profile picture of the user", guild_ids=vtacGuild)
-async def pfp(interaction: Interaction,user:nextcord.User = nextcord.SlashOption(name="user",description="Returns the profile picture of the user",required=True)):
-    await interaction.response.send_message(user.display_name + "'s profile picture:\n" + str(user.avatar.url))
+async def pfp(interaction: Interaction,member:nextcord.User = nextcord.SlashOption(name="user",description="Returns the profile picture of the user",required=True)):
+    await interaction.response.send_message(member.display_name + "'s profile picture:\n" + str(member.avatar.url))
 
 @bot.slash_command(name="info", description="Returns info of a user", guild_ids=vtacGuild)
-async def info(interaction: Interaction, user:nextcord.User = nextcord.SlashOption(name="user", description="Returns info of a user", required=True)):
-    info = "Joined guild on: " + user.joined_at.strftime("%A %B %d, %Y at %I:%M%p") + "\n"
-    info = info + "Account created on: " + user.created_at.strftime("%A %B %d, %Y at %I:%M%p")
+async def info(interaction: Interaction, member:nextcord.User = nextcord.SlashOption(name="user", description="Returns info of a user", required=True)):
+    info = "Joined guild on: " + member.joined_at.strftime("%A %B %d, %Y at %I:%M%p") + "\n"
+    info = info + "Account created on: " + member.created_at.strftime("%A %B %d, %Y at %I:%M%p")
     em = nextcord.Embed(title='', description=info, colour=0xFF0000)
-    em.set_author(name=user.display_name, icon_url=user.avatar.url)
+    em.set_author(name=member.display_name, icon_url=member.avatar.url)
     await interaction.response.send_message(embed=em)
 
 @bot.slash_command(name="addquote", description="Adds a quote to the database", guild_ids=vtacGuild)
 async def addquote(interaction: Interaction, 
-user: nextcord.User = nextcord.SlashOption(name="user", description="who said the funny?", required=True),
+member: nextcord.User = nextcord.SlashOption(name="user", description="who said the funny?", required=True),
 quote: str = nextcord.SlashOption(name="quote", description="the funny thing someone said", required=True)
 ):
-    register_quote(user, quote)
+    register_quote(member, quote)
     await interaction.response.send_message("Quote has been added :thumbsup:")
     load_quotes()
 
@@ -496,6 +341,39 @@ quote: str = nextcord.SlashOption(name="quote", description="the funny thing som
 async def quote(interaction: Interaction):
     await interaction.response.send_message(get_quote())
 
+@bot.slash_command(name="poke", description="Poke someone!", guild_ids=vtacGuild)
+async def poke(interaction: Interaction,member:nextcord.User = nextcord.SlashOption(name="user",description="person to poke",required=True)):
+    await interaction.response.send_message(interaction.user.mention + " just poked " + member.mention + "!", file=nextcord.File("img/poke.gif"))
+
+@bot.slash_command(name="flip", description="Flip a coin", guild_ids=vtacGuild)
+async def flip(interaction: Interaction):
+    if random.choice([True, False]) == True:
+        await interaction.response.send_message("Heads!")
+    else:
+        await interaction.response.send_message("Tails!")
+
+@bot.slash_command(name="hug", description="A hug, perhaps?", guild_ids=vtacGuild)
+async def hug(interaction: Interaction):
+    if random.choice([True, False]) == True:
+        await interaction.response.send_message(":hugging:")
+    else:
+        await interaction.response.send_message("No hug for you, cyka")
+
+@bot.slash_command(name="roll", description="Roll n amount of n-sided dice", guild_ids=vtacGuild)
+async def roll(interaction: Interaction,
+dice: str = nextcord.SlashOption(name="ndn", description="NdN format only", required=True)
+):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await interaction.response.send_message('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await interaction.response.send_message(result)
+      
+
+# Message listner
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
